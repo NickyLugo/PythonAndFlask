8027f55acb1cc76fea3f6a4085f21e14ce0bb408
Systems Engineer. Nicky Lugo <122755523+NickyLugo@users.noreply.github.com>
2023-06-02 20:22:26 -0600

    starting 4th practice
    Beverages warehouse in Python 3.11.

---
+++ Python Practice/AlmacenBebidas.py
@@ -0,0 +1,23 @@
+"""
+**Almacén de bebidas**
+
+Objeto:Bebida
+
+Parametros: 
+1) id_Bebida
+2) nombreBebida
+3) clasificacionBebida (ej. Agua, Bebidas Azucaradas, Bebidas energeticas)
+4) marcaBebida
+5) precioBebida
+
+Operaciones:
+alta()
+baja()
+actualizacion()
+mostrar()
+
+Funciones:
+A) Calcular Precio promedio de bebidas { ej. precioPromedio = [ sum(precios)/cantidad ] }
+B) Cantidad de bebidas de una Marca {Contar marcaBebida}
+C) Cantidad por clasificacion {print( "Agua: " + X + ", Bebidas Azucaradas: " + Y ) }
+"""
